0d4c2abf4d03cf8368ca433c12012888568c9beb
Jeff <zjyeung579@gmail.com>
2020-09-08 16:02:55 -0700

    Setup UI for signup page

--- src/App.js
+++ src/App.js
@@ -1,96 +1,102 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
-  Router,
+  Route,
   BrowserRouter as Router,
   Switch,
   Redirect
-} from 'react-router-dom';
-import Home from './pages/Home';
-import Chat from './pages/Chat';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import { auth } from './services/firebase';
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { auth } from "./services/firebase";
+import './styles.css';
 
 function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
-    {...rest}
-    render={(props) => authenticated === true
-      ? <Component {...props} />
-      : <Redirect to={{ pathname: '/login', state: { from: props.location } }} /> }
+      {...rest}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          )
+      }
     />
-  )
-};
+  );
+}
 
 function PublicRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
-      render={(props) => authenticated === false
-        ? <Component {...props} />
-        : <Redirect to='/chat'/> }
+      render={props =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/chat" />
+          )
+      }
     />
-  )
-};
+  );
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       authenticated: false,
-      loading: true,
+      loading: true
     };
   }
 
   componentDidMount() {
-    auth().onAuthStateChange((user) => {
+    auth().onAuthStateChanged(user => {
       if (user) {
         this.setState({
           authenticated: true,
-          loading: false,
+          loading: false
         });
       } else {
         this.setState({
           authenticated: false,
-          loading: false,
+          loading: false
         });
       }
-    })
+    });
   }
-
 
   render() {
-    return this.state.loading === true ? <h2>Loading...</h2> : (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PrivateRoute
-            path="/chat"
-            authenticated={this.state.authenticated}
-            component={Chat}
-          />
-          <PublicRoute
-            path="/signup"
-            authenticated={this.state.authenticated}
-            component={Signup}
-          />
-          <PublicRoute
-            path="/login"
-            authenticated={this.state.authenticated}
-            component={Login}
-          />
-        </Switch>
-      </Router>
-    );
+    return this.state.loading === true ? (
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute
+              path="/chat"
+              authenticated={this.state.authenticated}
+              component={Chat}
+            />
+            <PublicRoute
+              path="/signup"
+              authenticated={this.state.authenticated}
+              component={Signup}
+            />
+            <PublicRoute
+              path="/login"
+              authenticated={this.state.authenticated}
+              component={Login}
+            />
+          </Switch>
+        </Router>
+      );
   }
-};
-
-export function signup(email, password) {
-  return auth().createUserWithEmailAndPassword(email, password);
-};
-
-export function signin(email, password) {
-  return auth().signInWithEmailAndPassword(email, password);
-};
+}
 
 export default App;
---
+++ src/pages/Signup.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { signup } from '../helpers/auth';
+
+export default class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            email: '',
+            password: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <h1>
+                        Sign Up to
+                        <Link to="/">Chatroom</Link>
+                    </h1>
+                    <p>Fill in the form below to create an account.</p>
+                    <div>
+                        <input placeholder="Email" name="email" type="email" onChange={this.handleChange} value={this.state.email}></input>
+                    </div>
+                    <div>
+                        <input placeholder="Password" name="password" onChange={this.handleChange} value={this.state.password}></input>
+                    </div>
+                    <div>
+                        {this.state.error ? <p>{this.state.error}</p> : null}
+                        <button type="submit">Sign Up</button>
+                    </div>
+                    <hr></hr>
+                    <p>Already have an account? <Link to="/login">Login</Link></p>
+                </form>
+            </div>
+        );
+    }
+};
