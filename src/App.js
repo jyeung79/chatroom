7be52581c6956f0b0fc27f8567afe57b8668c4ad
Jeff <zjyeung579@gmail.com>
2020-09-08 16:45:18 -0700

    Setup Login screen with Google signin and Github signin

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Route,
   BrowserRouter as Router,
   Switch,
-  Redirect
+  Redirect,
 } from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import { auth } from "./services/firebase";
-import './styles.css';
+import Home from './pages/Home';
+import Chat from './pages/Chat';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { auth } from './services/firebase';
 
 function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
-      render={props =>
-        authenticated === true ? (
-          <Component {...props} />
-        ) : (
-            <Redirect
-              to={{ pathname: "/login", state: { from: props.location } }}
-            />
-          )
-      }
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
     />
-  );
+  )
 }
 
 function PublicRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
-      render={props =>
-        authenticated === false ? (
-          <Component {...props} />
-        ) : (
-            <Redirect to="/chat" />
-          )
-      }
+      render={(props) => authenticated === false
+        ? <Component {...props} />
+        : <Redirect to='/chat' />}
     />
-  );
+  )
 }
 
 class App extends Component {
@@ -54,48 +43,32 @@ class App extends Component {
   }
 
   componentDidMount() {
-    auth().onAuthStateChanged(user => {
+    auth().onAuthStateChanged((user) => {
       if (user) {
         this.setState({
           authenticated: true,
-          loading: false
+          loading: false,
         });
       } else {
         this.setState({
           authenticated: false,
-          loading: false
+          loading: false,
         });
       }
-    });
+    })
   }
 
   render() {
-    return this.state.loading === true ? (
-      <div className="spinner-border text-success" role="status">
-        <span className="sr-only">Loading...</span>
-      </div>
-    ) : (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <PrivateRoute
-              path="/chat"
-              authenticated={this.state.authenticated}
-              component={Chat}
-            />
-            <PublicRoute
-              path="/signup"
-              authenticated={this.state.authenticated}
-              component={Signup}
-            />
-            <PublicRoute
-              path="/login"
-              authenticated={this.state.authenticated}
-              component={Login}
-            />
-          </Switch>
-        </Router>
-      );
+    return this.state.loading === true ? <h2>Loading...</h2> : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute path="/chat" authenticated={this.state.authenticated} component={Chat}></PrivateRoute>
+          <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup}></PublicRoute>
+          <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login}></PublicRoute>
+        </Switch>
+      </Router>
+    );
   }
 }
 
