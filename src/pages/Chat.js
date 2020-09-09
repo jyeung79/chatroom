7726324d94f5b0efa9d3002224e5e6729370bfa4
Jeff <zjyeung579@gmail.com>
2020-09-08 19:23:44 -0700

    Added react createref for chat msgs

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -10,25 +10,30 @@ export default class Chat extends Component {
             chats: [],
             content: '',
             readError: null,
-            writeError: null
+            writeError: null,
+            loadingChats: false
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.myRef = React.createRef();
     }
 
     async componentDidMount() {
-        this.setState({ readError: null });
+        this.setState({ readError: null, loadingChats:true });
+        const chatArea = this.myRef.current;
         try {
-            db.ref('chats').on('value', snapshot => {
+            db.ref("chats").on("value", snapshot => {
                 let chats = [];
                 snapshot.forEach((snap) => {
                     chats.push(snap.val());
             });
             chats.sort((a, b) => a.timestamp - b.timestamp);
             this.setState({ chats });
+            chatArea.scrollBy(0, chatArea.scrollHeight);
+            this.setState({ loadingChats: false });
             });
         } catch (error) {
-            this.setState({ readError: error.message });
+            this.setState({ readError: error.message, loadingChats: false });
         
         }
     }
@@ -36,17 +41,19 @@ export default class Chat extends Component {
     async handleSubmit(event) {
         event.preventDefault();
         this.setState({ writeError: null });
+        const chatArea = this.myRef.current;
         try {
-            await db.ref('chats').push({
+            await db.ref("chats").push({
                 content: this.state.content,
                 timestamp: Date.now(),
-                uid: this.state.uid
+                uid: this.state.user.uid
             });
             this.setState({ content: ''});
+            chatArea.scrollBy(0, chatArea.scrollHeight);
         } catch (error) {
             this.setState({ writeError: error.message });
         }
-    };
+    }
 
     handleChange(event) {
         this.setState({
@@ -54,18 +61,33 @@ export default class Chat extends Component {
         });
     }
 
+    formatTime(timestamp) {
+        const d = new Date(timestamp);
+        const time = `${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+        return time;
+      }
+
     render() {
         return (
             <div>
-                <div className='chats'>
-                    {this.state.chats.map((chat) => {
-                        return <p key={chat.timestamp}>{chat.content}</p>
-                    })}
+                <div className="chats" ref={this.myRef}>
+                    {this.state.loadingChats ? 
+                        <div role="status">
+                            <span className="sr-only">Loading...</span> 
+                        </div>
+                    : ''}
                 </div>
-                {'message form'}
+                {this.state.chats.map((chat) => {
+                    return <p key={chat.timestamp} className="chat-bubble">
+                        {chat.content}
+                    <br />
+                    <span className="chat-time">{this.formatTime(chat.timestamp)}</span>
+                    </p>
+                })}
                 <form onSubmit={this.handleSubmit}>
-                    <input onChange={this.handleChange} value={this.state.content}></input>
+                    <textarea name="content" onChange={this.handleChange} value={this.state.content}></textarea>
                     {this.state.error ? <p>{this.state.writeError}</p> : null}
+                    <button type="submit">Send</button>
                 </form>
                 <div>
                     Login in as: <strong>{this.state.user.email}</strong>
