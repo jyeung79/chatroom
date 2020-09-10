cb54ceeab4a014c527a0e65b80eb8e83526632db
Jeff <zjyeung579@gmail.com>
2020-09-09 21:46:33 -0700

    Added minor styling to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,11 +12,11 @@ export default class HomePage extends Component {
         <section>
           <div className="jumbotron jumbotron-fluid py-5">
             <div className="container text-center py-5">
-              <h1 className="display-4">Welcome to Chatty</h1>
+              <h1 className="display-4">Welcome to Chatroom</h1>
               <p className="lead">A great place to share your thoughts with friends</p>
               <div className="mt-4">
                 <Link className="btn btn-primary px-5 mr-3" to="/signup">Create New Account</Link>
-                <Link className="btn px-5" to="/login">Login to Your Account</Link>
+                <Link className="btn px-5 btn-success" to="/login">Login to Your Account</Link>
               </div>
             </div>
           </div>
